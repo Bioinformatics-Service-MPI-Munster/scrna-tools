36f13b2ee044d05a9a12a9b800c4cd2fee105800
Kai Kruse <kai.kruse@mpi-muenster.mpg.de>
2024-07-03 08:00:51 +0200

    Conform to new Matplotlib organisation

--- scrna_tools/plotting/__init__.py
+++ scrna_tools/plotting/__init__.py
@@ -1,14 +1,22 @@
+from packaging.version import Version
 import matplotlib
 
 import matplotlib.colors as mcol
-import matplotlib.cm
+
+if Version(matplotlib.__version__) < Version('3.9.0'):
+    import matplotlib.cm as cm
+    register_cmap = cm.register_cmap
+else:
+    cm = matplotlib.colormaps
+    register_cmap = cm.register
+
 import matplotlib.pyplot as plt
 
 from .base_plotting import *
 from .embedding import *
 from .bar import *
 from .celloracle import *
-from ._deprecated import *
+# from ._deprecated import *
 from .helpers import *
 
 
@@ -18,14 +26,14 @@ for cmap_name, colors in [
     ('RedWhiteBlue', ["#B22727","#d0d0d0","#104e8b"]),
 ]:
     try:
-        matplotlib.cm.get_cmap(cmap_name)
+        cm.get_cmap(cmap_name)
     except ValueError:
-        cm = mcol.LinearSegmentedColormap.from_list(cmap_name, colors)
-        plt.register_cmap(name=cmap_name, cmap=cm)
+        cmap = mcol.LinearSegmentedColormap.from_list(cmap_name, colors)
+        register_cmap(name=cmap_name, cmap=cmap)
     
     try:
-        matplotlib.cm.get_cmap(f'{cmap_name}_r')
+        cm.get_cmap(f'{cmap_name}_r')
     except ValueError:
-        cm_r = mcol.LinearSegmentedColormap.from_list(f'{cmap_name}_r', colors[::-1])
-        plt.register_cmap(name=f'{cmap_name}_r', cmap=cm_r)
+        cmap_r = mcol.LinearSegmentedColormap.from_list(f'{cmap_name}_r', colors[::-1])
+        register_cmap(name=f'{cmap_name}_r', cmap=cmap_r)
 
